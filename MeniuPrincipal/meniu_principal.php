c6a0b32713fb10d4667ef544c37ab917a94d6281
Eduard <eduard_milotai@Kubuntu.myguest.virtualbox.org>
2024-04-07 13:43:38 +0300

    am adaugat animatie de hover pe elementele din navbar

--- MeniuPrincipal/meniu_principal.php
+++ MeniuPrincipal/meniu_principal.php
@@ -15,7 +15,7 @@
                 <img src="/Imagini/LogoDinamoBucuresti.png" alt="Logo" height="100" width="90">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fas fa-bars text-white"></i>
+                <i class="fas fa-bars text-black"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
@@ -34,6 +34,21 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <script>
+        $(document).ready(function () {
+            $('.navbar-toggler').click(function () {
+                // Schimbă clasa iconiței la click
+                var icon = $(this).find('i'); // Găsește elementul <i> din interiorul butonului
+                if (icon.hasClass('fa-bars')) {
+                    icon.removeClass('fa-bars').addClass('fa-times'); // Schimbă în X
+                } else {
+                    icon.removeClass('fa-times').addClass('fa-bars'); // Schimbă înapoi în hamburger
+                }
+            });
+        });
+    </script>
+
 
 </body>
 </html>
